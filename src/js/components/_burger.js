a206857e9767d466b3a74454978593f98a5d2cdc
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2024-05-13 01:05:23 +0300

    start

---
+++ src/js/components/_burger.js
@@ -0,0 +1,27 @@
+const navigation = document.querySelector('.nav');
+const page = document.querySelector('.page');
+const header = document.querySelector('.header');
+
+navigation ?.addEventListener('click', onBurgerBtnClick);
+
+function onBurgerBtnClick(e) {
+  if (!e.target.parentNode.matches('.burger') && !e.target.matches('.burger')) {
+    return;
+  }
+
+  this ?.classList.remove('closed');
+  page ?.classList.add('open');
+  header ?.classList.add('active');
+
+  if (this ?.matches('.opened')) {
+    this ?.classList.add('closed');
+    page ?.classList.remove('open');
+    header ?.classList.remove('active');
+
+    setTimeout(() => {
+      this ?.classList.remove('opened');
+    }, 100);
+  }
+
+  this ?.classList.add('opened');
+}
